5a55f307a67079a4bf80b06c9a1394336be64dad
Nibor9198 <robin.boregrim@gmail.com>
2016-10-26 13:08:53 +0200

    Loading fungerar men en Viewport null pointer exeption är ett problem, Fixa det robin. Jag tror på mig.

--- core/src/se/boregrim/gyarb/Game.java
+++ core/src/se/boregrim/gyarb/Game.java
@@ -8,6 +8,11 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import se.boregrim.gyarb.screens.LoadingScreen;
 import se.boregrim.gyarb.screens.MainMenu;
 
 import java.util.HashMap;
@@ -20,27 +25,34 @@ public class Game extends com.badlogic.gdx.Game {
 	AssetManager manager;
 
 	public BitmapFont font;
+	public FitViewport viewport;
+
 
 	@Override
 	public void create () {
 		//Initiating variables
 		batch = new SpriteBatch();
+		viewport = new FitViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		screens = new HashMap<String, Screen>();
 		assets = new Assets();
 		manager = assets.getAssetManager();
 		font = new BitmapFont();
 
 
-		manager.finishLoading();
+
+
 		MainMenu main = new MainMenu(this);
 		screens.put("main", main);
-		setScreen(main);
-
-
+		LoadingScreen load = new LoadingScreen(this);
+		screens.put("load", load);
+		setScreen(load);
 			}
 
 	@Override
 	public void render () {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
 		super.render();
 	}
 	
--- core/src/se/boregrim/gyarb/screens/MainMenu.java
+++ core/src/se/boregrim/gyarb/screens/MainMenu.java
@@ -5,7 +5,12 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import se.boregrim.gyarb.Assets;
 import se.boregrim.gyarb.Game;
 
@@ -19,11 +24,17 @@ public class MainMenu implements Screen {
     SpriteBatch batch;
     AssetManager manager;
     Game game;
-
+    GlyphLayout layout;
+    public Stage stage;
     public MainMenu(Game g) {
         this.batch = g.batch;
+        stage = new Stage();
+        FitViewport v = game.viewport;
+        stage.setViewport(game.viewport);
         manager = g.assets.getAssetManager();
         game = g;
+        layout = new GlyphLayout();
+
     }
 
     @Override
@@ -33,17 +44,17 @@ public class MainMenu implements Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
 
         float height = Gdx.graphics.getHeight();
         float width = Gdx.graphics.getWidth();
         batch.begin();
         //Texture t = manager.get("harambe2.png");
         //batch.draw(t,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
-
-        String s = "Ninja something";
-        game.font.draw(batch, s,width*0.5f - game.font.getSpaceWidth()* s.length(), height*0.75f);
+        layout = new GlyphLayout();
+        String s = "Title";
+        layout.setText(game.font,s);
+        game.font.draw(batch, s,width*0.5f - layout.width/2, height*0.75f);
 
         batch.end();
 
