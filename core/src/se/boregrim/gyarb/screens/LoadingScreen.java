0b986d1491d9e9797de75ec3a7fe48eee293d1c1
Nibor9198 <robin.boregrim@gmail.com>
2016-11-08 23:25:06 +0100

    Commented LoadningScreen

--- core/src/se/boregrim/gyarb/screens/LoadingScreen.java
+++ core/src/se/boregrim/gyarb/screens/LoadingScreen.java
@@ -25,34 +25,39 @@ public class LoadingScreen implements Screen {
 
     public LoadingScreen(Game game){
         this.game = game;
+        //Getting the assetmanager (is used as input for progressbar)
         manager = game.getAssets().getAssetManager();
-
+        // Creating the stage
         stage = new Stage();
+        //Getting the skin for the progressbar
         skin = game.getSkin();
 
     }
     @Override
     public void show() {
-
+        //Setting stage viewport
         stage.setViewport(game.getViewport());
 
+        //Creating Progressbar
         pb = new ProgressBar(0f,1f,0.1f,false,game.getSkin());
-
         pb.setBounds(stage.getWidth() * 0.5f -100f, stage.getHeight() * 0.5f,200,50);
+        //Adding it to the stage
         stage.addActor(pb);
 
-        //manager.finishLoadingAsset("harambe.png");
 
 
     }
 
     @Override
     public void render(float delta) {
+        // If all the assets are loaded, set screen to main
         if (manager.update()) {
             game.setScreen(game.getScreens().get("main"));
         }
-
+        //Update the Progressbar
         pb.setValue(manager.getProgress());
+
+        //Draw the stage
         stage.act();
         stage.draw();
 
@@ -60,8 +65,8 @@ public class LoadingScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
+        //Update viewport on resize
         stage.getViewport().update(width,height);
-
     }
 
     @Override
