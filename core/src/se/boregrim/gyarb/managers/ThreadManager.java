3565de5919f4ef7839eb151633476adfa451e647
Nibor9198 <robin.boregrim@gmail.com>
2016-12-13 15:55:03 +0100

    Small things

---
+++ core/src/se/boregrim/gyarb/managers/ThreadManager.java
@@ -0,0 +1,16 @@
+package se.boregrim.gyarb.managers;
+
+/**
+ * Created by Robin on 2016-12-11.
+ */
+public class ThreadManager {
+
+
+    public class PathfindingThread implements Runnable {
+
+        @Override
+        public void run() {
+
+        }
+    }
+}
--- core/src/se/boregrim/gyarb/pathfinding/GraphImp.java
+++ core/src/se/boregrim/gyarb/pathfinding/GraphImp.java
@@ -2,14 +2,35 @@ package se.boregrim.gyarb.pathfinding;
 
 import com.badlogic.gdx.ai.pfa.Connection;
 import com.badlogic.gdx.ai.pfa.Graph;
+import com.badlogic.gdx.ai.pfa.indexed.IndexedGraph;
 import com.badlogic.gdx.utils.Array;
 
 /**
  * Created by Robin on 2016-12-08.
  */
-public class GraphImp implements Graph{
-    @Override
-    public Array<Connection> getConnections(Object fromNode) {
-        return null;
-    }
-}
+//public class GraphImp implements IndexedGraph<Node>{
+//    protected Array<Node> nodes = new Array<Node>();
+//    protected int capacity;
+//
+//    public GraphImp(){
+//        super();
+//    }
+//    public GraphImp(int capacity){
+//        this.capacity = capacity;
+//    }
+//
+//    @Override
+//    public int getIndex(Node node) {
+//        return nodes.indexOf(node,true);
+//    }
+//
+//    @Override
+//    public int getNodeCount() {
+//        return nodes.size;
+//    }
+//
+//    @Override
+//    public Array<Connection<Node>> getConnections(Node fromNode) {
+//        return fromNode.getConnections();
+//    }
+//}
